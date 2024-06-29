fbf01b2fb92b0220863bc5fb69d27452779d4fc4
Eng.Abdu khalid yahya Al_hrazi <82823586+almomyz@users.noreply.github.com>
2024-06-29 06:31:28 +0300

    addaa

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+export const BASE_URL = process.env.REACT_APP_BASE_URL || "https://bestpricebackend1-kprqlej9.b4a.run";
 export const BASE_URL_IMAGE = `${BASE_URL}/public/images/`;
